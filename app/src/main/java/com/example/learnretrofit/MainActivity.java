8491c36c53493383d4feabdc327d90db7ca6e13c
Quang Ha <hakun32@gmail.com>
2022-02-25 10:45:45 +0700

    C3_25_02

--- app/src/main/java/com/example/learnretrofit/MainActivity.java
+++ app/src/main/java/com/example/learnretrofit/MainActivity.java
@@ -1,7 +1,8 @@
 
 package com.example.learnretrofit;
 
-import android.support.v7.app.AppCompatActivity;
+//import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
